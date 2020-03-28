3b0ca9603f13da67bebc05936ceb63d3bfaedafd
Mikel Barcina <mbarcina001@gmail.com>
2020-03-28 13:12:37 +0100

    Added mysql DB with Docker

---
+++ mysql/sql-scripts/01-TablesCreation.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user(
+    id int NOT NULL AUTO_INCREMENT,
+    username varchar(50),
+    password varchar(64),
+    email varchar(50),
+    PRIMARY KEY(id)
+)
---
+++ mysql/sql-scripts/02-DataInsertion.sql
@@ -0,0 +1,3 @@
+INSERT INTO user (username, password, email) VALUES 
+    ('admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'admin@kraken.com'), 
+    ('test', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 'test@kraken.com')
