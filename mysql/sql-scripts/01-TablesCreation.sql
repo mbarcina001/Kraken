5631eadc9f776001493f013c468eac8c5eaad8dc
Mikel <mbarcina001@gmail.com>
2020-04-26 16:49:02 +0200

    Added roles to DB

--- mysql/sql-scripts/01-TablesCreation.sql
+++ mysql/sql-scripts/01-TablesCreation.sql
@@ -1,7 +1,20 @@
-CREATE TABLE user(
+CREATE TABLE User(
     id int NOT NULL AUTO_INCREMENT,
-    username varchar(50),
+    name varchar(50),
     password varchar(64),
     email varchar(50),
     PRIMARY KEY(id)
-)
+);
+
+CREATE TABLE Role(
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(20),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE User_Role(
+    user_id int,
+    role_id int,
+    FOREIGN KEY (user_id) REFERENCES User (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (role_id) REFERENCES Role (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
--- mysql/sql-scripts/02-DataInsertion.sql
+++ mysql/sql-scripts/02-DataInsertion.sql
@@ -1,3 +1,11 @@
-INSERT INTO user (username, password, email) VALUES 
-    ('admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'admin@kraken.com'), 
-    ('test', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 'test@kraken.com')
+INSERT INTO user (id, name, password, email) VALUES 
+    (1, 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'admin@kraken.com'), 
+    (2, 'test', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 'test@kraken.com');
+
+INSERT INTO role(id, name) VALUES
+    (1, 'admin'),
+    (2, 'user');
+
+INSERT INTO userrole(user_id, role_id) VALUES
+    (1, 1),
+    (2, 2);
