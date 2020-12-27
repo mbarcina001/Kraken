20ff12121facdab7469461b0aa3e6a54d2571811
Mikel <mbarcina001@gmail.com>
2020-12-27 14:21:20 +0100

    Logger

--- api/src/main/java/io/mbarcina/kraken/api/KrakenApiApplication.java
+++ api/src/main/java/io/mbarcina/kraken/api/KrakenApiApplication.java
@@ -1,5 +1,7 @@
 package io.mbarcina.kraken.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -8,8 +10,12 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @EntityScan(basePackages = {"io.mbarcina.kraken.auth.entity", "io.mbarcina.kraken.api.entity"})
 public class KrakenApiApplication 
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KrakenApiApplication.class);
+
     public static void main( String[] args )
     {
     	SpringApplication.run(KrakenApiApplication.class, args);
+    	
+        LOGGER.info("Simple log statement with inputs {}, {} and {}", 1,2,3);
     }
 }
